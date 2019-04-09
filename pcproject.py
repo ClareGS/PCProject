d4ebf77135261d3f4ece7fc4a8092d2a0b086e9b
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2019-04-09 21:19:38 +0100

    User gets error message if they enter a letter

--- pcproject.py
+++ pcproject.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request
-import logging
 import requests
 import json
-import time
-import urllib
+import math
+import re
 
 flask_app = Flask(__name__)
 
@@ -14,7 +13,11 @@ def hello():
 @flask_app.route("/currency", methods=["POST"])
 def get_pounds():
     form_data = request.form["num"]
-    return calculate_amt_euros(form_data)
+    if re.search('[a-zA-Z]', form_data) != None:
+        return " Oops! Just numbers please"
+    else:
+        result = calculate_amt_euros(form_data)
+        return render_template("page2.html", result=result)
 
 def get_exchange_rate():
     endpoint = "https://openexchangerates.org/api/latest.json?app_id=8567d4e0f026487db09bafbfbfda2069&base=GBP"
